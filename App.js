bc8dee0c1c85ed6e2f29e36b6d7412e27e1be7b6
Amy <jiang.yujun42@gmail.com>
2018-02-26 19:19:46 -0500

    change from expo init project

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
+// import { FBLogin, FBLoginManager } from 'react-native-facebook-login';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
